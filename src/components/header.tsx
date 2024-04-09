6f418108a7a01c5434bc38200d406e9f6f577d33
mohamed ibourki <mohamedibourkioffice@gmail.com>
2024-04-09 00:35:17 +0000

    add navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,12 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
+import { Button } from "./ui/button";
 
 export default function Header() {
     return (
         <div className="relative w-full flex items-center justify-center">
             <Navbar className="top-2" />
-            <p className="text-black dark:text-white">
-                The Navbar will show on top of the page
-            </p>
         </div>
     );
 }
@@ -21,6 +19,14 @@ function Navbar({ className }: { className?: string }) {
             className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
         >
             <Menu setActive={setActive}>
+                <MenuItem setActive={setActive} active={active} item="Home">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/web-dev">Web Development</HoveredLink>
+                        <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+                        <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+                        <HoveredLink href="/branding">Branding</HoveredLink>
+                    </div>
+                </MenuItem>
                 <MenuItem setActive={setActive} active={active} item="Services">
                     <div className="flex flex-col space-y-4 text-sm">
                         <HoveredLink href="/web-dev">Web Development</HoveredLink>
@@ -57,7 +63,23 @@ function Navbar({ className }: { className?: string }) {
                         />
                     </div>
                 </MenuItem>
-                <MenuItem setActive={setActive} active={active} item="Pricing">
+                <MenuItem setActive={setActive} active={active} item="Categories">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/hobby">Hobby</HoveredLink>
+                        <HoveredLink href="/individual">Individual</HoveredLink>
+                        <HoveredLink href="/team">Team</HoveredLink>
+                        <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Account">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/hobby">Hobby</HoveredLink>
+                        <HoveredLink href="/individual">Individual</HoveredLink>
+                        <HoveredLink href="/team">Team</HoveredLink>
+                        <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Cart">
                     <div className="flex flex-col space-y-4 text-sm">
                         <HoveredLink href="/hobby">Hobby</HoveredLink>
                         <HoveredLink href="/individual">Individual</HoveredLink>
