683cfee4af314944f071f59cd8042c4a62df2eb5
mohamed ibourki <mohamedibourkioffice@gmail.com>
2024-04-15 23:45:13 +0100

    init the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='bg-zinc-800 h-screen text-white pt-28'>
+        <div className=''>
+            <div className='LeftSide'>
+                <h1>Macbook 14 Pro</h1>
+                <p>
+                    My apologies for the verbosity! In short, the MacBook 14 Pro is a sleek, powerful, and portable laptop with a 14-inch Retina display, the latest Apple silicon chips, versatile ports, and a stylish design, making it a top choice for professionals and creatives alike.
+                </p>
+                <button>Read More</button>
+                <button>Add To Cart</button>
+            </div>
+        </div>
+    </div>
+  )
+}
